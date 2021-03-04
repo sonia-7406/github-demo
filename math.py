1885ae213ca16f2a53adeb5c426ef63b08f368ff
sonia <immanuelsonia@gmail.com>
2021-03-04 11:53:13 +0530

    Sub implemetation

--- math.py
+++ math.py
@@ -4,7 +4,11 @@ def add(x,y):
 
 # Subtract implementation
 def subtract(x,y):
-    return x-y
+    if x>y:
+        return ERROR
+    else:
+        return pass
+
 
 # Multiply implementation
 def multiply(x,y):
