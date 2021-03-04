68896d794f04d72f139b0e47af472c399fc0d06f
sonia-7406 <77523157+sonia-7406@users.noreply.github.com>
2021-03-04 11:33:31 +0530

    Initializing math file

---
+++ math.py
@@ -0,0 +1,20 @@
+# Add implementation
+def add(x,y):
+	pass
+
+# Subtract implementation
+def subtract(x,y):
+    pass
+
+# Multiply implementation
+def multiply(x,y):
+        if x>y:
+            pass
+
+# Divide implementation
+def divide(x,y):
+        if x>y:
+            return 1
+        else
+            return ERROR
+    pass
